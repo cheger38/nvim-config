95e311f0d8d3576608d0a07539e6834114161290
cheger38 <charlieheger500@gmail.com>
2026-05-17 16:12:05 -0500

    removed LICENSE and plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+vim.opt.colorcolumn = "85"
 vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 8
+vim.opt.sidescrolloff = 2
 vim.opt.clipboard = ""
